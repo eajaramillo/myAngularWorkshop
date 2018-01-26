ff14edb3eeec3fbe954e218d8b295660be267138
Edwar Jaramillo <eajs565@gmail.com>
2018-01-26 18:55:51 -0500

    TypeScriptSE6

---
+++ TypeScriptES6/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var mensaje = "Hola";
+if (true) {
+    var mensaje = "Adios!!";
+}
+console.log(mensaje);
+// test 1
+// function saludar(nombre:string){
+//   console.log("Hola "+nombre);
+// }
+//
+// var wolverine = {
+//   nombre: "Logan"
+// }
+//
+// saludar(wolverine.nombre.toUpperCase());
---
+++ TypeScriptES6/app.ts
@@ -0,0 +1,17 @@
+var mensaje = "Hola";
+
+if(true){
+  var mensaje = "Adios!!";
+}
+console.log(mensaje);
+
+// test 1
+// function saludar(nombre:string){
+//   console.log("Hola "+nombre);
+// }
+//
+// var wolverine = {
+//   nombre: "Logan"
+// }
+//
+// saludar(wolverine.nombre.toUpperCase());
