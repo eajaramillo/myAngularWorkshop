69f7e1ef2460a13a1282ac5994c2f5dfcbec1c4a
Edwar Jaramillo <eajs565@gmail.com>
2018-02-12 13:47:11 -0500

    Utilizar el campo search del navbar para realizar una búsqueda
    Se crea un nuevo componente para mostrar los resiltados de la búsqueda.
    
    En las rutas agrego la nueva ruta de búsqueda pasando un parámetro que será el texto buscado.
    
    El nuevo componente importa el ActivedRoute para tomar los parámetros de la ruta y el servicio de heroes para acceder al método en el servicio que hace efectiva la búsqueda.
    
    En el template del componente de búsqueda puedo utilizar componente html propio para mostrar los resultados o también puedo reutilizar en esta caso el componente heroes.component.html para mostrar sólo los elementos como resultado de la búsqueda.

---
+++ 6.Angular/2. SPA/src/app/components/buscar/buscar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: '../heroes/heroes.component.html'
+  // templateUrl: './buscar.component.html'
+})
+export class BuscarComponent implements OnInit {
+  heroes:Heroe[]=[];
+  constructor(private _activatedRouter:ActivatedRoute,
+              private _heroesService:HeroesService) {
+  }
+
+  ngOnInit() {
+    this._activatedRouter.params.subscribe(params=>{
+      this.heroes = this._heroesService.buscarHeroe(params['termino']);
+    });
+  }
+
+}
