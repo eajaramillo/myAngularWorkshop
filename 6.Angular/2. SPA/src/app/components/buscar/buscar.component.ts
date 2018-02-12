ce5199337e24cedf732564ce2d440af236f6f46a
Edwar Jaramillo <eajs565@gmail.com>
2018-02-12 13:59:57 -0500

    Mostrar un mensaje cuando no existen resultados con el termino deseado
    Se hace uso de ngIf para mostrar un div dependiente de la cantidad de elementos del arreglo de heroes.

--- 6.Angular/2. SPA/src/app/components/buscar/buscar.component.ts
+++ 6.Angular/2. SPA/src/app/components/buscar/buscar.component.ts
@@ -4,11 +4,12 @@ import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
-  templateUrl: '../heroes/heroes.component.html'
-  // templateUrl: './buscar.component.html'
+  // templateUrl: '../heroes/heroes.component.html'
+  templateUrl: './buscar.component.html'
 })
 export class BuscarComponent implements OnInit {
   heroes:Heroe[]=[];
+  termino:string="";
   constructor(private _activatedRouter:ActivatedRoute,
               private _heroesService:HeroesService) {
   }
@@ -16,6 +17,7 @@ export class BuscarComponent implements OnInit {
   ngOnInit() {
     this._activatedRouter.params.subscribe(params=>{
       this.heroes = this._heroesService.buscarHeroe(params['termino']);
+      this.termino = params['termino'];
     });
   }
 
