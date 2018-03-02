6d0f8c314db0eb104e41576112af3d9eead460e4
Edwar Jaramillo <eajs565@gmail.com>
2018-03-02 14:13:39 -0500

    Directiva ngClass
    Creando un nuevo componente para aprender y aplicar la directiva ngClass

---
+++ 6.Angular/6. Miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+  alerta:string = 'alert-danger';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- 6.Angular/6. Miscelaneos/src/app/components/css/css.component.ts
+++ 6.Angular/6. Miscelaneos/src/app/components/css/css.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Los estilos definidos en el template solo afectan
+// al mismo template, a menos que se defina una estilo global
+// en el archivo de estilos style.css
 @Component({
   selector: 'app-css',
   template: `
