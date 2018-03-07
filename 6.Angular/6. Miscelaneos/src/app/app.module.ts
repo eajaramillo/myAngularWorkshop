50a1a2191f9e632d695158cd1fccaf5f3feb6143
Edwar Jaramillo <eajs565@gmail.com>
2018-03-07 14:10:10 -0500

    Rutas y rutas hijas
    Se implementa un navbar para tener la posibilidad de aplicar el concepto de rutas y ahora se aplica se concepto de rutas hijas, además se crea un componente de rutas hijas como un componente en especial con el fin de separar la definición de rutas hijas de un componente de las rutas generales.
    
    El navbar contiene básicamente dos rutas generales (Home y Usuario), el componente usuario a su vez tiene un submenú que redirecciona a rutas hijas por ejemplo para crear nuevo usuario, editar o ver detalle.
    
    Se crea usuario.routes.ts donde se defienen las rutas de usuario.

--- 6.Angular/6. Miscelaneos/src/app/app.module.ts
+++ 6.Angular/6. Miscelaneos/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+//Rutas
+import { APP_ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
@@ -8,6 +11,12 @@ import { CssComponent } from './components/css/css.component';
 import { ClasesComponent } from './components/clases/clases.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
 import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -17,10 +26,18 @@ import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
     CssComponent,
     ClasesComponent,
     ResaltadoDirective,
-    NgSwitchComponent
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
