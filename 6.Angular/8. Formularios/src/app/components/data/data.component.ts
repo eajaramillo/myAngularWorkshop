bed0d904fd58f46c2b04e99aba9e3483dfaa84c2
Edwar Jaramillo <eajs565@gmail.com>
2018-03-15 14:09:41 -0500

    Aproximación de formulario por data (código desde el componente)
    Enlazar inputs a propiedades del FormGroup y realizar validaciones.
    
    Formas acceder a los parámetros de un formGroup.
    
    Agrupaciones de objetos (FormGroupName), subObjetos en el FormGroup.

--- 6.Angular/8. Formularios/src/app/components/data/data.component.ts
+++ 6.Angular/8. Formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
 export class DataComponent implements OnInit {
+  forma:FormGroup;
 
-  constructor() { }
+  usuario:Object = {
+    nombrecompleto : {
+      nombre:"Edwar",
+      apellido:"Jaramillo"
+    },
+    correo:"eajs565@gmail.com"
+  };
+  constructor() {
+    console.log(this.usuario);
+    this.forma = new FormGroup({
+      'nombrecompleto':new FormGroup({
+        'nombre':new FormControl('', [
+                                      Validators.required,
+                                      Validators.minLength(3)
+                                    ]),
+        'apellido':new FormControl('', Validators.required)
+      }),
+      'correo':new FormControl('', [
+                                    Validators.required,
+                                    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                  ])
+    });
+  }
 
   ngOnInit() {
+  }
+
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma);
   }
 
 }
