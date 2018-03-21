d1d68757fab918ea08a62b6cb16d0e4f365b4ccc
Edwar Jaramillo <eajs565@gmail.com>
2018-03-21 14:18:55 -0500

    Otra forma de realizar validaciones y validación asíncrona
    Como se ha hecho ántes, la validación se hizo directamente en el FormControl de cada dato, pero también esta la posibilidad de hacerlo de la siguiente manera (por medio de setValidators):
    
    Despues de la definición del FormGroup
    
    this.forma.controls['password2'].setValidators([
          Validators.required,
          this.noIgual.bind(this.forma)//noIgual es una validación personalizada
        ]);
    
    Existe también la validación asíncrona por medio de promesas, donde el formulario será inválido hasta tanto la promesa se ejecute y retorne un valor, ejemplo.
    
    existeUsuario(control:FormControl):Promise<any>|Observable<any>{
        let promesa = new Promise(
          (resolve,reject)=>{
            setTimeout(()=>{
              if(control.value === "edwar"){
                resolve({existe:true})
              }else{
                resolve(null)
              }
            },3000);
          }
        );
    
        return promesa;
      } // El setTimeout en este ejemplo simula el tiempo que tarda en consultar el servidor.
    
    Es necesario importar:
    import { Observable } from 'rxjs/Rx';

--- 6.Angular/8. Formularios/src/app/components/data/data.component.ts
+++ 6.Angular/8. Formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
@@ -16,6 +18,7 @@ export class DataComponent implements OnInit {
     correo:"eajs565@gmail.com",
     //pasatiempos: ['Correr','Dormir','Correr']
   };
+  
   constructor() {
     console.log(this.usuario);
     this.forma = new FormGroup({
@@ -35,13 +38,49 @@ export class DataComponent implements OnInit {
                                   ]),
       'pasatiempos':new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'username':new FormControl('', //Valor por defecto
+                                Validators.required,// Validadores síncronos
+                                this.existeUsuario// Validadores asíncronos
+                                ),
+      'password1':new FormControl('',Validators.required),
+      'password2':new FormControl('',Validators.required)
     });
 
     // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ]);
+
+    this.forma.controls['username'].valueChanges
+        .subscribe(data=>{
+          console.log(data);
+        });
+
+    this.forma.controls['username'].statusChanges
+        .subscribe(data=>{
+          console.log(data);
+        });
   }
 
   ngOnInit() {
+  }
+
+  existeUsuario(control:FormControl):Promise<any>|Observable<any>{
+    let promesa = new Promise(
+      (resolve,reject)=>{
+        setTimeout(()=>{
+          if(control.value === "edwar"){
+            resolve({existe:true})
+          }else{
+            resolve(null)
+          }
+        },3000);
+      }
+    );
+
+    return promesa;
   }
 
   guardarCambios(){
@@ -69,7 +108,16 @@ export class DataComponent implements OnInit {
         noTextoValido:true
       }
     }
+    return null;
+  }
 
+  noIgual(control:FormControl):{[s:string]:boolean} {
+    let forma:any = this;
+    if(control.value !== forma.controls['password1'].value){
+      return {
+        noiguales:true
+      }
+    }
     return null;
   }
 
